3bdde73edcc88aead6eb923b4876f33bcb5bb61f
NANJUNDARADHYA H Y <117804917+pillamma@users.noreply.github.com>
2025-08-22 19:05:54 +0530

    Create quaternions.h
    header for io of quaternions

---
+++ quaternions.h
@@ -0,0 +1,10 @@
+struct Quaternion {
+    // The real part of the quaternion
+    float w;
+    // The i component of the vector part
+    float x;
+    // The j component of the vector part
+    float y;
+    // The k component of the vector part
+    float z;
+};
