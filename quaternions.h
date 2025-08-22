29bc9f9ffcda12fe67eff1811948c161f3f36bc8
NanjundaradhyaHY <nanjundaradhyahy@RUDRA@gmail.com>
2025-08-22 19:32:58 +0530

    added funtions

--- quaternions.h
+++ quaternions.h
@@ -1,4 +1,4 @@
-struct Quaternion {
+struct quaternion {
     // The real part of the quaternion
     float w;
     // The i component of the vector part
@@ -8,3 +8,7 @@ struct Quaternion {
     // The k component of the vector part
     float z;
 };
+
+void quaternion_multiply(struct Quaternion* q1, struct Quaternion* q2, struct Quaternion* result);
+float norm(const struct Quaternion* q);
+inverse( struct Quaternion* q,struct Quaternion* result);
