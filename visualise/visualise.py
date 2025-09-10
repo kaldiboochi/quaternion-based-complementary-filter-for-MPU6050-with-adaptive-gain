14c7762a4a766983fd9499eb8f198c096456f9d6
NANJUNDARADHYA H Y <117804917+kaldiboochi@users.noreply.github.com>
2025-09-11 00:50:30 +0530

    the c code was correct i.e implementation but the visualisation was having jerks as euler angles are messy they behave oddly at singularity

--- visualise/visualise.py
+++ visualise/visualise.py
@@ -1,6 +1,7 @@
 import threading, time, queue, ctypes, serial, sys, math
 from vpython import vector, box, arrow, rate, scene, color, compound, cos , sin , cross
 from quaternion_ctypes import Quaternion, qlib
+from quaternion import quaternion
 import numpy as np
 data_q = queue.Queue(maxsize=1)
 quat_q = queue.Queue(maxsize=1)
@@ -20,12 +21,16 @@ def filter_thread():
         try:
             line = data_q.get()
             vals = [float(x) for x in line.split(',')]
+            #vals = [0,0,0,1,1,1]
             if len(vals) == 6:
                 now = time.time()
                 dt = now - last
                 last = now
                 mg = (ctypes.c_float*3)(*vals[3:])
                 ac = (ctypes.c_float*3)(*vals[:3])
+                #for w in mg:
+                #    print(f"{round(float(w),2)}",end="     ")
+                #print("w")
                 qptr = ctypes.byref(q)
                 qlib.cf_update_from_gyro(qptr, mg, dt)
                 qlib.cf_update_from_accel(qptr, ac)
@@ -43,7 +48,7 @@ def main(port):
     toRad=2*np.pi/360
     toDeg=1/toRad
     scene.forward=vector(-1,-1,-1)
-
+    scene.up=vector(0,0,1)
     scene.width=1200
     scene.height=1080
 
@@ -59,7 +64,9 @@ def main(port):
     bn=box(length=1,width=.75,height=.1, pos=vector(-.5,.1+.05,0),color=color.blue)
     nano=box(lenght=1.75,width=.6,height=.1,pos=vector(-2,.1+.05,0),color=color.green)
     myObj=compound([bBoard,bn,nano])
-
+    px = quaternion(0, 1, 0, 0)
+    py = quaternion(0, 0, 1, 0)
+    pz = quaternion(0, 0, 0, 1)
     try:
         while True:
             q = quat_q.get()
@@ -67,21 +74,20 @@ def main(port):
             q1=q[1]
             q2=q[2]
             q3=q[3]
-
-            pitch=-math.atan2(2*(q0*q1+q2*q3),1-2*(q1*q1+q2*q2))
-            roll=math.asin(2*(q0*q2-q3*q1))
-            yaw=math.atan2(2*(q0*q3+q1*q2),1-2*(q2*q2+q3*q3))+np.pi/2
-            k=vector(cos(yaw)*cos(pitch), sin(pitch),sin(yaw)*cos(pitch))
-            y=vector(0,1,0)
-            s=cross(k,y)
-            v=cross(s,k)
-            vrot=v*cos(roll)+cross(k,v)*sin(roll)
-
-            frontArrow.axis=k
-            sideArrow.axis=cross(k,vrot)
-            upArrow.axis=vrot
-            myObj.axis=k
-            myObj.up=vrot
+            att = quaternion(q0, q1, q2, q3)
+            c_att = att.conjugate()
+            px_rot = (att * px * c_att).conjugate()
+            py_rot = (att * py * c_att).conjugate()
+            pz_rot = (att * pz * c_att).conjugate()
+            
+            x=vector(px_rot.x,px_rot.y,px_rot.z)
+            y=vector(py_rot.x,py_rot.y,py_rot.z)
+            z=vector(pz_rot.x,pz_rot.y,pz_rot.z)
+            frontArrow.axis=x
+            sideArrow.axis=y
+            upArrow.axis=z
+            myObj.axis=y
+            myObj.up=z
             sideArrow.length=2
             frontArrow.length=4
             upArrow.length=1
@@ -98,4 +104,3 @@ def main(port):
 if __name__ == "__main__":
     port = sys.argv[1] if len(sys.argv)>1 else "/dev/ttyACM0"
     main(port)
-
